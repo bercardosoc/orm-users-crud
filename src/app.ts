9f83add620622301c98109fa9f237c31309534dd
bercardosoc <bernardocardosodacosta@gmail.com>
2022-06-10 14:30:28 -0300

    feat: add cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from "express"
 import { routes } from "./routers/user.routers"
 
+const cors = require("cors")
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 app.use(routes)
 
 app.listen(3333)
